1819485613b9daeed4152631ac5fb048fe1a575a
DESKTOP-Q4NR7LP\caizonglin <171900662@qq.com>
2019-10-23 22:29:59 +0800

    完成ini配置信息的读写类开发

--- SummaryTable/Helper/RegexConfigurer.cs
+++ SummaryTable/Helper/RegexConfigurer.cs
@@ -10,6 +10,14 @@ namespace SummaryTable.Helper
 {
     public class RegexConfigurer
     {
+        public static string iniConfigPath;//正则规则ini配置文件目录
+        private IniFile IniHelper;
+
+        public RegexConfigurer()
+        {
+            iniConfigPath = System.Environment.CurrentDirectory + "\\RegexRule.ini";
+            IniHelper = new IniFile(iniConfigPath);
+        }
 
 
         /// <summary>
@@ -17,9 +25,16 @@ namespace SummaryTable.Helper
         /// </summary>
         /// <param name="key">配置节点key值</param>
         /// <returns>包含对应节点信息的实体类对象</returns>
-        public RegexRule GetRegexRule(string key)
+        public RegexRule GetRegexRule(string SectionName)
         {
             RegexRule regexRule = new RegexRule();
+            regexRule.Code = IniHelper.ReadString(SectionName, "Code", "未能正确读取__(n,m)__评估编号");
+            regexRule.ValueTime = IniHelper.ReadString(SectionName, "ValueTime", "未能正确读取__(n,m)__价值时点");
+            regexRule.Customer = IniHelper.ReadString(SectionName, "Customer", "未能正确读取__(n,m)__委托方名称");
+            regexRule.Location = IniHelper.ReadString(SectionName, "Location", "未能正确读取__(n,m)__项目坐落");
+            regexRule.ArchitecherArea = IniHelper.ReadString(SectionName, "ArchitecherArea", "未能正确读取__(n,m)__建筑面积");
+            regexRule.SingleValue = IniHelper.ReadString(SectionName, "SingleValue", "未能正确读取__(n,m)__评估单价");
+            regexRule.TotalValue = IniHelper.ReadString(SectionName, "TotalValue", "未能正确读取__(n,m)__评估总价");
             return regexRule;
         }
 
@@ -30,109 +45,36 @@ namespace SummaryTable.Helper
         /// </summary>
         /// <param name="regexRule"></param>
         /// <returns></returns>
-        public string SetRegexRule(RegexRule regexRule)
+        public bool SetRegexRule(string SectionName,RegexRule regexRule)
         {
-
-            return "";
+            try
+            {
+                IniHelper.WriteString(SectionName, "Code", regexRule.Code);
+                IniHelper.WriteString(SectionName, "ValueTime", regexRule.ValueTime);
+                IniHelper.WriteString(SectionName, "Customer", regexRule.Customer);
+                IniHelper.WriteString(SectionName, "Location", regexRule.Location);
+                IniHelper.WriteString(SectionName, "ArchitecherArea",regexRule.ArchitecherArea);
+                IniHelper.WriteString(SectionName, "SingleValue", regexRule.SingleValue);
+                IniHelper.WriteString(SectionName, "TotalValue", regexRule.TotalValue);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
         }
 
-
-        public void testit()
+        public void temptest()
         {
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            var mySection = config.GetSection("mySection") as SummaryTable.Helper.MySection;
-            foreach (SummaryTable.Helper.MySection.MyKeyValueSetting add in mySection.KeyValues)
-            {
-                Console.WriteLine(string.Format("{0}-{1}", add.Key, add.Value));
-            }
-        }
-    }
+            IniHelper.WriteString("正式报告模板", "报告编号0", "000000");
+            IniHelper.WriteString("正式报告模板", "报告编号1", "111111");
+            IniHelper.WriteString("正式报告模板", "报告编号2", "222222");
 
+            IniHelper.WriteString("预评估", "报告编号0", "0");
+            IniHelper.WriteString("预评估", "报告编号1", "1");
+            IniHelper.WriteString("预评估", "报告编号2", "8888888888888");
 
-
-
-
-
-
-
-
-
-
-    /// <summary>
-    /// 自定义节点配置类
-    /// </summary>
-    public class MySection : ConfigurationSection    // 所有配置节点都要选择这个基类
-    {
-        private static ConfigurationProperty s_property = new ConfigurationProperty(string.Empty, typeof(MyKeyValueCollection), null, ConfigurationPropertyOptions.IsDefaultCollection);
-
-        [ConfigurationProperty("", Options = ConfigurationPropertyOptions.IsDefaultCollection)]
-        public MyKeyValueCollection KeyValues
-        {
-            get
-            {
-                return (MyKeyValueCollection)base[s_property];
-            }
-        }
-        [ConfigurationCollection(typeof(MyKeyValueSetting))]
-        public class MyKeyValueCollection : ConfigurationElementCollection        // 自定义一个集合
-        {
-            // 基本上，所有的方法都只要简单地调用基类的实现就可以了。
-            public MyKeyValueCollection()
-                : base(StringComparer.OrdinalIgnoreCase)    // 忽略大小写
-            {
-
-            }
-
-            // 其实关键就是这个索引器。但它也是调用基类的实现，只是做下类型转就行了。
-            new public MyKeyValueSetting this[string name]
-            {
-                get { return (MyKeyValueSetting)base.BaseGet(name); }
-                set { base[name] = value; }
-            }
-
-            // 下面二个方法中抽象类中必须要实现的。
-            protected override ConfigurationElement CreateNewElement()
-            {
-                return new MyKeyValueSetting();
-            }
-
-            protected override object GetElementKey(ConfigurationElement element)
-            {
-                return ((MyKeyValueSetting)element).Key;
-            }
-
-            // 说明：如果不需要在代码中修改集合，可以不实现Add, Clear, Remove
-            public void Add(MyKeyValueSetting setting)
-            {
-                this.BaseAdd(setting);
-            }
-
-            public void Clear()
-            {
-                base.BaseClear();
-            }
-
-            public void Remove(string name)
-            {
-                base.BaseRemove(name);
-            }
-        }
-        public class MyKeyValueSetting : ConfigurationElement    // 集合中的每个元素
-        {
-
-            [ConfigurationProperty("key", IsRequired = true)]
-            public string Key
-            {
-                get { return this["key"].ToString(); }
-                set { this["key"] = value; }
-            }
-
-            [ConfigurationProperty("value", IsRequired = true)]
-            public string Value
-            {
-                get { return this["value"].ToString(); }
-                set { this["value"] = value; }
-            }
+            IniHelper.ReadString("预评估", "报告编号2","defalt");
         }
 
     }
